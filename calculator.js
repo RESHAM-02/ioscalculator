d2512122697f47eb8e09993fb0e9bbeea010e270
RESHAM-02 <resharmaa2202@gmail.com>
2026-02-03 11:36:28 +0545

    commit ioscalculator

---
+++ calculator.js
@@ -0,0 +1,27 @@
+
+let exp = "0" ;
+
+const calculations = document.getElementById('calculations');
+
+function display(value) {
+    exp = exp === "0" ? value : exp + value;
+    calculations.innerText = exp;
+    console.log(exp)
+}
+
+function evaluation() {
+    const result = eval(exp);
+    console.log(result);
+    calculations.innerText = result;
+    exp = "0";
+}
+
+function reset() {
+    exp = "0";
+    calculations.innerText = exp;
+}
+
+function removeLast() {
+      exp = exp.slice(0, -1) || "0"
+      calculations.innerText = exp;
+}
